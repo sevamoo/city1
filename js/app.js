724bc440fab10f112c5f162e8265e2741a4f865e
Vahid Moosavi <sevamoo@gmail.com>
2017-01-27 09:45:40 +0100

    Some commit message

--- js/app.js
+++ js/app.js
@@ -322,6 +322,8 @@ var lngs = {"0":-159.5872222,"1":-104.4644112,"2":-99.4415007,"3":-101.7526723,"
 	}).addTo(mymap14);
 	var credits = L.control.attribution().addTo(mymap14);
 	credits.addAttribution('<a href="https://www.mapbox.com/map-feedback/">Mapbox</a>');
+	attribution = mymap14.attributionControl;
+	attribution.setPrefix('');
 
 
 
